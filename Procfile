b41428550cee19f1c533b19c2033d52d283d3417
Peter Komaromi <kompeet1@gmail.com>
2020-11-20 13:57:37 +0000

    Deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn boutiqu_ado.wsgi:application
--- boutiqu_ado/settings.py
+++ boutiqu_ado/settings.py
@@ -18,12 +18,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9+e^gnjg@_hx6j$k89&5m(cll3^*1zwaghc0@()kxoms@vdk8b'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kompeet-boutiqu-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -116,14 +116,16 @@ WSGI_APPLICATION = 'boutiqu_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
-}
-
-
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
